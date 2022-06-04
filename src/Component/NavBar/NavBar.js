fc916efb243f995392e77699f2e3c67f49fbe001
Mohsin-Hossain <mohsinshawon18@gmail.com>
2022-06-04 13:25:43 +0600

    header fixed

---
+++ src/Component/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div>
+            <a href="home">Home</a>
+            <a href="review">Review</a>
+            <a href="dashboard">Dashboard</a>
+            <a href="blogs">Blogs</a>
+            <a href="about">About</a>
+        </div>
+    );
+};
+
+export default NavBar;
