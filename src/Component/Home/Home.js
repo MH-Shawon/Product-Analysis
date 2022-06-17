eb4316aa36379191dbb4ed903c1a443d923fe6e8
Mohsin-Hossain <mohsinshawon18@gmail.com>
2022-06-17 23:43:42 +0600

    images addead

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './Home.css'
+import Images from './images/DSC.jpeg'
+
+
 const Home = () => {
     return (
         <div className='home'>
+            <div className='description'>
             <h1>
                 You Select <br />
                 We Collect!!!!
             </h1>
+            <p>
+            Everyone wanted something that would push their performance to the next-level, and it was out job to rise to the challenge. Now as a family of master bat makers along with a skilled team of local artisans, we create arguably the best cricket equipment on the planet, which you know as DSC.
+            </p>
+            </div>
             
+            <div className='img'>
+            <img src={Images} alt="" />
+            </div>
         </div>
     );
 };
